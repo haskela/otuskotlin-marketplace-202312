839c9d626190a7bab969e515659802475266d613
haskela <gemay.alexa@gmail.com>
2024-02-11 02:51:20 +0300

    m2l10: add composite builds and build plugins

--- build-plugin/src/main/kotlin/BuildPluginMultiplatform.kt
+++ build-plugin/src/main/kotlin/BuildPluginMultiplatform.kt
@@ -9,6 +9,7 @@ import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.the
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
 @Suppress("unused")
 internal class BuildPluginMultiplatform : Plugin<Project> {
     override fun apply(project: Project) = with(project) {
@@ -34,7 +35,7 @@ internal class BuildPluginMultiplatform : Plugin<Project> {
 @Suppress("LongMethod", "MagicNumber")
 private fun KotlinMultiplatformExtension.configureTargets(project: Project) {
     val libs = project.the<LibrariesForLibs>()
-        targets {
+    targets {
         jvmToolchain {
             languageVersion.set(JavaLanguageVersion.of(libs.versions.jvm.language.get()))
         }
